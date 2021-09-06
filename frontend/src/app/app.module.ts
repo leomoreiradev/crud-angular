f218ae687412504414e15f6be0099a5b657d4dbc
leomoreiradev <leandrodev.oliveira@gmail.com>
2021-09-05 21:15:15 -0300

    Criação de formulario

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -43,7 +46,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [], //Se eu tiver um service que eu queira expor pra fora do mudulo, eu coloco ele aqui em providers
   bootstrap: [AppComponent]
